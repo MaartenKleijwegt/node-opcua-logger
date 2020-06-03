603fbf12f25353130efad101b9c36a8fc82b3d96
Joris de Kruijff | Barth Industrial Automation <jorisdekruijff@barthgroep.nl>
2020-06-03 07:38:03 +0200

    Added comments for better readability

--- src/timescaledb.js
+++ src/timescaledb.js
@@ -6,7 +6,8 @@ let conf = config.load();
 
 
 const log = require('./logging.js').getLogger('main')
-// input database, username and password vars in resp. places
+// input database, username and password vars in resp. place
+// --> geautomatiseerd d.m.v. config file! Zie config.toml
 
 var connection = new Sequelize(conf.timescaledb.database, conf.timescaledb.username, conf.timescaledb.password, {
 host: conf.timescaledb.host,
@@ -15,6 +16,8 @@ dialect: "postgres",
 });
 
 // Model for OPC UA database values
+// Gebaseerd op Sequelize
+// Code bouwt een tabel met de juiste columns
 var value1 = connection.define('values', {
   timestamp:{
     type: Sequelize.DATE,
@@ -43,8 +46,7 @@ var value1 = connection.define('values', {
 
 );
 
-var boolcheck = "";
-
+// Functie die bij de opstart controleert of de database bereikbaar is
 async function start () {
 
   try {
@@ -59,20 +61,18 @@ async function start () {
 
 
 
+// Functie die dient voor het schrijven van de eerder gedefinieerde tabel-velden naar de database.
+// Genereert ook nieuwe tabel indien nog niet aanwezig.
+// Future Async Promise: loopt asyncroon van de rest: kan schrijven wanneer nodig.
+// Krijgt waardes wanneer deze beschikbaar zijn.
+// Schrijft deze waardes vervolgens weg.
+
+//TODO naam veranderen: value1 --> iets zinvollers
 
 async function write (points) {
   let pts = points.map((p) => {
     let tags = p.tags || {}
 
-
-    boolcheck = p.value
-    if (boolcheck === "true") {
-      boolcheck = 1;
-    } else if(boolcheck == "false") {
-      boolcheck = 0;
-    } else {
-      boolcheck = p.value
-    };
 
     connection.sync().then(function() {
       try {
@@ -93,5 +93,7 @@ async function write (points) {
   })
   })
 }
+
+// Exporteert de start en write functies zodat deze kunnen worden gebruikt in andere delen van de code.
 
 module.exports = { start, write }
